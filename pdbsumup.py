ac16b17ec1c1874871a2a42aa17c4f5ea443e4d6
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-05-29 21:38:28 +0200

    Colored output removed 	modified:   pdbsumup.py

--- pdbsumup.py
+++ pdbsumup.py
@@ -12,17 +12,6 @@ from pymol import cmd
 PDBFILENAME = sys.argv[1]
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 def ruler(char='-', length=32):
     print(char * length)
 
@@ -40,12 +29,12 @@ for chain in chains:
     natoms = cmd.select(f'inpdb and chain {chain}')
     nres_per_chain.append(nres)
     natoms_per_chain.append(natoms)
-    print(bcolors.BOLD + f'chain {chain}' + bcolors.ENDC)
+    print(f'chain {chain}')
     print(f'number or residues:\t{nres}')
     print(f'number or atoms:\t{natoms}')
 ruler('#', length=64)
 nres_per_chain = numpy.asarray(nres_per_chain)
 natoms_per_chain = numpy.asarray(natoms_per_chain)
-print(bcolors.BOLD + f'Total number of chain:\t\t{len(chains)}')
+print(f'Total number of chain:\t\t{len(chains)}')
 print(f'Total number of residues:\t{nres_per_chain.sum()}')
-print(f'Total number of atoms:\t\t{natoms_per_chain.sum()}' + bcolors.ENDC)
+print(f'Total number of atoms:\t\t{natoms_per_chain.sum()}')
